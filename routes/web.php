c41c288f1fa698878f8a3647e795556ea7d48cec
farhanexe <farhanfalah790@gmail.com>
2025-08-19 23:35:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/login/admin', function () {
+    return view('login_admin'); // nama view HARUS sama dengan nama file blade (tanpa .blade.php)
+})->name('login.admin');
+
+Route::get('/dashboard/admin', function () {
+    return "Selamat datang di Dashboard Admin!";
+})->name('admin.dashboard');
+Route::get('/', function () {
+    return view('home'); // ini file home.blade.php
+})->name('home');
